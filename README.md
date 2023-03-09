93d355895faf7e4eb3fa57b91bcb3e4e8cafca09
algolabs-kh <kkh93@algorithmlabs.co.kr>
2023-03-09 16:50:20 +0900

    fix name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 👷 Graphy
+# 👷 Graphier
 
 Currently Developing!
 
--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='graphy',
-    version='0.0.1',
+    name='graphier',
+    version='0.0.0.1',
     description="This library aims to visualize relationships among a large amount of variables via graph-based approach",
     author='Keonho Kim',
     author_email='keonhok0315@gmail.com',
     url='https://github.com/keonho-kim/graphy',
     install_requires=['pandas', 'numpy', 'networkx', 'scipy', 'scikit-learn', 'matplotlib'],
     packages=find_packages(exclude=[]),
-    keywords=['graph analysis', 'visualization', 'eda', 'exploratory analysis']
-    python_requires='>=3.6',
-    package_data={},
+    python_requires='>=3.7',
     zip_safe=False,
     entry_points={
         "console_scripts":
@@ -20,10 +18,11 @@ setup(
             ]
     },
     classifiers=[
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     package_data={},
     include_package_data=True
