3cc5b3227d3ad18699fac36888afd7b03d886e77
algolabs-kh <kkh93@algorithmlabs.co.kr>
2023-03-09 13:22:45 +0900

    minor fix

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="graphy",
-    version="0.0.1",
-    description="This library aims to visualize relationships among a large amount of variables with graph-based approach",
-    author="Keonho Kim",
-    author_email="keonhok0315@gmail.com",
+    name='graphy',
+    version='0.0.1',
+    description="This library aims to visualize relationships among a large amount of variables via graph-based approach",
+    author='Keonho Kim',
+    author_email='keonhok0315@gmail.com',
     url='https://github.com/keonho-kim/graphy',
-    python_requires=">=3.8",
-    packages=find_packages(),
-    install_requires=['pandas', 'numpy', 'networkx', 'scipy', 'matplotlib'],
+    install_requires=['pandas', 'numpy', 'networkx', 'scipy', 'scikit-learn', 'matplotlib'],
+    packages=find_packages(exclude=[]),
+    keywords=['graph analysis', 'visualization', 'eda', 'exploratory analysis']
+    python_requires='>=3.6',
+    package_data={},
     zip_safe=False,
     entry_points={
         "console_scripts":
@@ -17,6 +19,13 @@ setup(
              "graphy = graphy.main:main"   
             ]
     },
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
     package_data={},
     include_package_data=True
 )
+
